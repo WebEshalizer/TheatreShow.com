08ebc097780e6756a754b1ef89b4c4b4c965bb99
Eshal <asadeshal14@gmil.com>
2024-02-21 20:30:34 +0500

    added all files

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.getElementById("myForm").addEventListener("submit", function(event) {
+//     event.preventDefault();
+  
+//     // Add your form submission logic here
+//     // For example, you can use Fetch API to send form data to a server
+//     // and handle the response.
+//     // Replace the following lines with your actual form submission code.
+  
+//     alert("Form submitted!");
+//   });
+  
